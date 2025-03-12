78ce7f6c0987e29e867472f06d31c9a2a1bb367a
Sunleader <sunyaxing_1997@163.com>
2025-03-12 14:49:00 +0800

    模拟输入

---
+++ transflow-plugins/plugin-demo-input/src/main/java/org/sunyaxing/transflow/plugindemoinput/DemoInputExt.java
@@ -0,0 +1,54 @@
+package org.sunyaxing.transflow.plugindemoinput;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.pf4j.Extension;
+import org.sunyaxing.transflow.TransData;
+import org.sunyaxing.transflow.extensions.TransFlowInput;
+import org.sunyaxing.transflow.extensions.base.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+
+@Extension
+public class DemoInputExt extends TransFlowInput {
+    private static final Logger log = LogManager.getLogger(DemoInputExt.class);
+    private String jsonStr;
+
+    public DemoInputExt(ExtensionContext extensionContext) {
+        super(extensionContext);
+        log.info("create");
+    }
+
+    @Override
+    public void commit(Long offset) {
+//        log.info("提交偏移量 {}", offset);
+    }
+
+    @Override
+    public List<TransData> dequeue() {
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        List<TransData> transData = new ArrayList<>();
+        transData.add(new TransData(0L, jsonStr));
+        return transData;
+    }
+
+    @Override
+    public void run() {
+    }
+
+    @Override
+    public void init(Properties config) {
+        this.jsonStr = config.getProperty("jsonStr", "{\"param\":\"1\"}");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
---
+++ transflow-plugins/plugin-demo-input/src/main/java/org/sunyaxing/transflow/plugindemoinput/DemoInputPlugin.java
@@ -0,0 +1,6 @@
+package org.sunyaxing.transflow.plugindemoinput;
+
+import org.sunyaxing.transflow.plugins.TransFlowInputPlugin;
+
+public class DemoInputPlugin extends TransFlowInputPlugin {
+}
